014512d9c9fc7ed8ca1d136e249a8b14b2d451df
hare1039 <hare39393@gmail.com>
2024-08-21 09:45:41 -0700

    untabify

--- sketch_aug11c.ino
+++ sketch_aug11c.ino
@@ -9,48 +9,46 @@ int constexpr startPORT     = 8; // variable => power on --> Run program, power 
 int constexpr fastRIGHT     = 9; // variable => power on --> Run conti. right; power off --> unuseful
 
 // Variables
-int constexpr waitpd = 1000*5; //collect sample time in millisecond
-int constexpr pd     = 50;     //Pulse Delay period in millisecond, pulse/pulse duration
-int constexpr frpd   = 500;    //fast right
+int constexpr waitpd = 1000*5; // collect sample time in millisecond
+int constexpr pd     = 50;     // Pulse Delay period in millisecond, pulse/pulse duration
+int constexpr frpd   = 500;    // fast right
 bool          setdir = LOW;    // Set Direction
 int           wpd    = waitpd; // initial collection/waiting time
 
 //Interrupt Handler
-
 void revmotor ()
 {
-	setdir = !setdir;
+    setdir = !setdir;
 }
 void fastspeed ()
 {
-	wpd = frpd;
+    wpd = frpd;
 }
 void samplespeed ()
 {
-	wpd = waitpd;
+    wpd = waitpd;
 }
-
 
 void setup()
 {
-	Serial.begin(9600);
-	pinMode (driverDIR, OUTPUT);
-	pinMode (driverPUL, OUTPUT);
-	pinMode (startPORT, INPUT);
-	pinMode (fastRIGHT, INPUT);
-	attachInterrupt(digitalPinToInterrupt(reverseSwitch), revmotor, FALLING);
-	attachInterrupt(digitalPinToInterrupt(fastRIGHT), fastspeed, CHANGE);
-	attachInterrupt(digitalPinToInterrupt(fastRIGHT), samplespeed, CHANGE);
+    Serial.begin(9600);
+    pinMode (driverDIR, OUTPUT);
+    pinMode (driverPUL, OUTPUT);
+    pinMode (startPORT, INPUT);
+    pinMode (fastRIGHT, INPUT);
+    attachInterrupt(digitalPinToInterrupt(reverseSwitch), revmotor, FALLING);
+    attachInterrupt(digitalPinToInterrupt(fastRIGHT), fastspeed, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(fastRIGHT), samplespeed, CHANGE);
 }
 void loop()
 {
-	for (int i = 0; i < 200*100; i++)
-	{
-		digitalWrite(driverDIR, setdir);
-		digitalWrite(driverPUL, HIGH);
-		delayMicroseconds(pd);
-		digitalWrite(driverPUL, LOW);
-		delayMicroseconds(pd);
-	}
-	delay(wpd);
+    for (int i = 0; i < 200*100; i++)
+    {
+        digitalWrite(driverDIR, setdir);
+        digitalWrite(driverPUL, HIGH);
+        delayMicroseconds(pd);
+        digitalWrite(driverPUL, LOW);
+        delayMicroseconds(pd);
+    }
+    delay(wpd);
 }
